832174b33d1f5ec6aa385679b8a4f4065be7b672
Paul-Ngigi <paulkush7777@gmail.com>
2021-04-18 21:51:26 +0300

    add instance relative configuration

--- app/__init__.py
+++ app/__init__.py
@@ -3,10 +3,11 @@ from .config import DevConfig
 from flask_bootstrap import Bootstrap
 
 # Initializing the app
-app = Flask(__name__)
+app = Flask(__name__, instance_relative_config=True)
 
 # Setting up configuration
 app.config.from_object(DevConfig)
+app.config.from_pyfile('config.py')
 
 # Initializing flask extensions
 bootstrap = Bootstrap(app)
