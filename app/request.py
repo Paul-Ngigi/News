58bfa1c2dbbb2580002385e664cca5e2fa55de68
Paul-Ngigi <paulkush7777@gmail.com>
2021-04-18 22:02:53 +0300

    add the urls and api key in the request file

---
+++ app/request.py
@@ -0,0 +1,8 @@
+from app import app
+
+# Getting api key 
+api_key = app.config['API_KEY']
+
+# Getting the base urls
+sources_base_url = ['SOURCES_BASE_URL']
+headlines_base_url = ['HEADLINES_BASE_URL']
