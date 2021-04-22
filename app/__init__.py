3974a629060577fa13ab1978e79d19db3f56c85e
Paul-Ngigi <paulkush7777@gmail.com>
2021-04-22 07:07:43 +0300

    add the news html file

--- app/__init__.py
+++ app/__init__.py
@@ -4,18 +4,18 @@ from config import config_options
 
 bootstrap = Bootstrap()
 
+
 def create_app(config_name):
     app = Flask(__name__)
-    #creating app configurations
+    # creating app configurations
     app.config.from_object(config_options[config_name])
 
-    #initialising flask extensions
+    # initialising flask extensions
     bootstrap.init_app(app)
 
-    #registering the blueprint created in app/__init__.py
+    # registering the blueprint created in app/__init__.py
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
     from .request import configue_request
     configue_request(app)
     return app
-     
